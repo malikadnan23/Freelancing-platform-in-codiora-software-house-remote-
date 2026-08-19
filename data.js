eadac84eb75c4f7001434475e72365fe0e4a746f
adnan saeed <121759955+malikadnan23@users.noreply.github.com>
2026-08-19 14:06:02 +0500

    first commit

---
+++ data.js
@@ -0,0 +1,6 @@
+const howitworks = [
+    {id:1,img:"/images/fe 1.png",title:"Post a Job",desc:"It’s free and easy to post a job. Simply fill in a title, description."},
+    {id:2,img:"/images/fe 2.png",title:"Post a Job",desc:"It’s free and easy to post a job. Simply fill in a title, description."},
+    {id:3,img:"/images/fe 3.png",title:"Post a Job",desc:"It’s free and easy to post a job. Simply fill in a title, description."},
+    {id:4,img:"/images/fe 4.png",title:"Post a Job",desc:"It’s free and easy to post a job. Simply fill in a title, description."}
+]
